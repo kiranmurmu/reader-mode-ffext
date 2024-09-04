45669de13a7d4dfd9aa328d5a1b5eb707b5d1769
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2024-09-04 11:16:54 +0530

    Restructure content script code

--- src/lib/content-script.ts
+++ src/lib/content-script.ts
@@ -5,21 +5,22 @@ declare var chrome: Default.Browser;
 declare var browser: Default.Browser;
 
 (function () {
-    /** if browser not found then use chrome */
+    type MessageData = { textContent: string };
+    type ResponseCallback = (response: unknown) => void;
+
     if (typeof browser == "undefined") {
         globalThis.browser = chrome;
     }
 
-    type RequestData = { textContent: string };
-    type ResponseCallback = (response: unknown) => void;
-
-    function replaceBodyContent(message: RequestData, _sender: Runtime.MessageSender, _sendResponse?: ResponseCallback) {
+    function handleMessageEvent(message: MessageData, _sender: Runtime.MessageSender, _sendResponse?: ResponseCallback) {
         document.body.textContent = "";
 
         let header = document.createElement("h1");
         header.textContent = message.textContent;
         document.body.appendChild(header);
+        
+        // TODO: update about:page url using `window.history`
     }
 
-    browser.runtime.onMessage.addListener(replaceBodyContent);
+    browser.runtime.onMessage.addListener(handleMessageEvent);
 })();
